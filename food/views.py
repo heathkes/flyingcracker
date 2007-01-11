2aa7e7f0544c33f138210d4817449c217cea4b6f
graham <devnull@localhost>
2007-01-11 07:51:38 +0000

    Retrieved ingredients for a recipe, ordered by rank. Displaying ingredients in the recipe detail view.

--- food/views.py
+++ food/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 from fc3.food.models import Recipe
 from django.shortcuts import render_to_response, get_object_or_404
+import operator
 
 def index(request):
     all_recipes = Recipe.objects.all().order_by('title')
@@ -8,4 +9,14 @@ def index(request):
     
 def detail(request, recipe_slug):
     r = get_object_or_404(Recipe, slug=recipe_slug)
-    return render_to_response('food/detail.html', {'recipe' : r})    
+    
+    # get ingredients for this recipe
+    ingredient_list = []
+    for i in r.ingredient_set.all().order_by('rank'):
+        ingredient_dict = {}
+        ingredient_dict['title'] = i.foodstuff.title
+        ingredient_dict['quantity'] = i.quantity
+        ingredient_dict['modifier'] = i.modifier
+        ingredient_list.append(ingredient_dict)
+    
+    return render_to_response('food/detail.html', {'recipe' : r, 'ingredients' : ingredient_list})    
