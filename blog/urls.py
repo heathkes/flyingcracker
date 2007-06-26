57f669d9f8abc23349c6c8d4db3d0320c203f2a6
graham <devnull@localhost>
2007-06-26 22:41:35 +0000

    Showing all posts for a given year.

--- blog/urls.py
+++ blog/urls.py
@@ -10,6 +10,6 @@ urlpatterns = patterns('django.views.generic',
     (r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\w{1,2})/(?P<slug>[0-9A-Za-z-]+)/$', 'date_based.object_detail', dict(blog_dict, month_format='%m', slug_field='slug')),
     (r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\w{1,2})/$', 'date_based.archive_day',   dict(blog_dict, month_format='%m')),
     (r'^(?P<year>\d{4})/(?P<month>\d{2})/$',                    'date_based.archive_month', dict(blog_dict, month_format='%m')),
-    (r'^(?P<year>\d{4})/$',                                     'date_based.archive_year',  blog_dict),
+    (r'^(?P<year>\d{4})/$',                                     'date_based.archive_year',  dict(blog_dict, make_object_list=True),
     (r'^/?$',                                                   'date_based.archive_index', blog_dict),
  )
