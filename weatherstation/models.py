f5ef3d2850cafd8b168f9e5d64118efaa83c72e2
graham <devnull@localhost>
2007-10-18 22:59:49 +0000

    Better handling of input weather data. Now if an input specifies the same timestamp as an existing record, the existing data is overwritten. Also now allowing the omission of any data in a record. If any part of the input is invalid an exception is raised and all the provided data is ignored.

--- weatherstation/models.py
+++ weatherstation/models.py
@@ -5,17 +5,17 @@ class Weather(models.Model):
     station_id = models.CharField(maxlength=10)
     timestamp = models.DateTimeField('Date of reading', unique=True)
    
-    wind_dir = models.IntegerField()
-    wind_speed = models.DecimalField(max_digits=5, decimal_places=2)
-    wind_peak = models.DecimalField(max_digits=5, decimal_places=2)
-    humidity = models.IntegerField()
-    temp = models.DecimalField(max_digits=5, decimal_places=2)
-    rain = models.DecimalField(max_digits=4, decimal_places=2)
-    barometer = models.DecimalField(max_digits=4, decimal_places=2)
-    dewpoint = models.DecimalField(max_digits=4, decimal_places=2)
-    temp_inside = models.DecimalField(max_digits=5, decimal_places=2)
-    baro_trend = models.DecimalField(max_digits=3, decimal_places=2)
-    windchill = models.DecimalField(max_digits=5, decimal_places=2)
+    wind_dir = models.IntegerField(null=True)
+    wind_speed = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    wind_peak = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    humidity = models.IntegerField(null=True)
+    temp = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    rain = models.DecimalField(max_digits=4, decimal_places=2, null=True)
+    barometer = models.DecimalField(max_digits=4, decimal_places=2, null=True)
+    dewpoint = models.DecimalField(max_digits=4, decimal_places=2, null=True)
+    temp_inside = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    baro_trend = models.DecimalField(max_digits=3, decimal_places=2, null=True)
+    windchill = models.DecimalField(max_digits=5, decimal_places=2, null=True)
 
     def __str__(self):
         if self.timestamp is not None:
--- weatherstation/views.py
+++ weatherstation/views.py
@@ -15,32 +15,65 @@ def upload_data(request):
         none
     """
 
-    try:
-        dateutc = request.GET.get('datemtn')
+    dateutc = request.GET.get('datemtn', None)
+    if dateutc is not None:
         dateutc = dateutc.replace(':',' ').replace('-',' ').replace('+',' ')
         year,month,day,hour,minute,second = dateutc.split()
         mountain_tz = USTimeZone(-7, "Mountain", "MST", "MDT")
         timestamp = datetime(int(year),int(month),int(day),int(hour),int(minute),int(second),0,mountain_tz)
         
-        rec = Weather.objects.create(
-                         wind_dir = request.GET.get('winddir'),
-                         wind_speed = request.GET.get('windspeedmph'),
-                         wind_peak = request.GET.get('windgustmph'),
-                         temp = request.GET.get('tempf'),
-                         barometer = request.GET.get('baromin'),
-                         dewpoint = request.GET.get('dewptf'),
-                         humidity = request.GET.get('humidity'),
-                         temp_inside = request.GET.get('tempinf'),
-                         baro_trend = request.GET.get('baromtrendin'),
-                         windchill = request.GET.get('windchillf'),
-                         rain = request.GET.get('rainin'),
-                         timestamp = timestamp,
-                         station_id = request.GET.get('ID'),
-                        )
-    except:
-        response=HttpResponse("failed: '%s'" % traceback.format_exc())
+        wind_dir    = request.GET.get('winddir', None)
+        wind_speed  = request.GET.get('windspeedmph', None)
+        wind_peak   = request.GET.get('windgustmph', None)
+        temp        = request.GET.get('tempf', None)
+        barometer   = request.GET.get('baromin', None)
+        dewpoint    = request.GET.get('dewptf', None)
+        humidity    = request.GET.get('humidity', None)
+        temp_inside = request.GET.get('tempinf', None)
+        baro_trend  = request.GET.get('baromtrendin', None)
+        windchill   = request.GET.get('windchillf', None)
+        rain        = request.GET.get('rainin', None)
+        station_id  = request.GET.get('ID', None)
+
+        try:
+            rec,created = Weather.objects.get_or_create(
+                             timestamp = timestamp,
+                             defaults = {
+                                 'wind_dir': wind_dir,
+                                 'wind_speed': wind_speed,
+                                 'wind_peak': wind_peak,
+                                 'temp': temp,
+                                 'barometer': barometer,
+                                 'dewpoint': dewpoint,
+                                 'humidity': humidity,
+                                 'temp_inside': temp_inside,
+                                 'baro_trend': baro_trend,
+                                 'windchill': windchill,
+                                 'rain': rain,
+                                 'station_id': station_id,
+                             }
+                            )
+            if created is not True:
+                # Record already exists, replace with good values and save.
+                rec.wind_dir = int(wind_dir),
+                rec.wind_speed = float(wind_speed),
+                rec.wind_peak = float(wind_peak),
+                rec.temp = float(temp),
+                rec.barometer = float(barometer),
+                #rec.dewpoint = dewpoint,
+                rec.humidity = int(humidity),
+                rec.temp_inside = float(temp_inside),
+                #rec.baro_trend = baro_trend,
+                rec.windchill = float(windchill),
+                #rec.rain = rain,
+                rec.station_id = station_id,
+                rec.save()
+        except:
+            response=HttpResponse("failed: '%s'" % traceback.format_exc())
+        else:
+            response = HttpResponse("success")
     else:
-        response = HttpResponse("success")
+        response = HttpResponse("failed: missing dateutc field in URL")
     return response
     
 def download_data(request):
