5b39d701acb9e18cb6144358845636b6a1e8e3a5
graham <devnull@localhost>
2007-10-18 17:48:49 +0000

    Added template block for holding javascript includes. Added code to weather view to support AJAX on the server side.

--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render_to_response
+from django.http import HttpResponse
 from fc3.weatherstation.models import Weather
+from django.utils import simplejson
 
 def current(request):
     # get latest weather reading
     current = Weather.objects.latest('timestamp')
-    return render_to_response('weather/current.html', {'current' : current})
+    
+    xhr = request.GET.has_key('xhr')
+    if xhr:
+        return HttpResponse(simplejson.dumps(current), mimetype='application/javascript')
+    else:
+        return render_to_response('weather/current.html', {'current' : current})
