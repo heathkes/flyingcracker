8621bce7dd3bf53f50e2bd1a4b7ecd56e64b1cf5
graham <devnull@localhost>
2007-10-30 20:51:20 +0000

    updated comment BugzID: 9

--- static/js/yuitextareas.js
+++ static/js/yuitextareas.js
@@ -1,7 +1,7 @@
 var myEditor = new YAHOO.widget.Editor('id_body', { 
 	    height: '600px', 
 	    width: '100%',
-        handleSubmit: true,
+        handleSubmit: true, // automatically saves the HTML in the textarea on submit
 	    dompath: true, //Turns on the bar at the bottom 
 	    animate: true //Animates the opening, closing and moving of Editor windows 
 	}); 
