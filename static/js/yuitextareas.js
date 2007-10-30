66e2bcc0094ec863853e24ad9440c4305942aea4
graham <devnull@localhost>
2007-10-30 17:58:05 +0000

    Added editor instantiation parameter to automatically save HTML on submit. BugzID: 9

--- static/js/yuitextareas.js
+++ static/js/yuitextareas.js
@@ -1,13 +1,8 @@
 var myEditor = new YAHOO.widget.Editor('id_body', { 
 	    height: '600px', 
-	    width: '100%', 
+	    width: '100%',
+        handleSubmit: true,
 	    dompath: true, //Turns on the bar at the bottom 
 	    animate: true //Animates the opening, closing and moving of Editor windows 
 	}); 
 myEditor.render();
-
-YAHOO.util.Event.on('post_form', 'submit', function(ev) {
-        YAHOO.util.Event.stopEvent(ev);
-        myEditor.saveHTML();
-        YAHOO.util.Dom.get('post_form').submit();
-});
