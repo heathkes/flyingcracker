4de5c8b52ce47c865484aac7a009af1672f0ba2f
graham <devnull@localhost>
2007-10-30 06:12:05 +0000

    Added a small bit of code which listens for a SUBMIT event for the Post form, saves the cleaned-up HTML to the textarea, and resubmits the form. BugzID: 5

--- static/js/yuitextareas.js
+++ static/js/yuitextareas.js
@@ -4,4 +4,10 @@ var myEditor = new YAHOO.widget.Editor('id_body', {
 	    dompath: true, //Turns on the bar at the bottom 
 	    animate: true //Animates the opening, closing and moving of Editor windows 
 	}); 
-myEditor.render(); 
+myEditor.render();
+
+YAHOO.util.Event.on('post_form', 'submit', function(ev) {
+        YAHOO.util.Event.stopEvent(ev);
+        myEditor.saveHTML();
+        YAHOO.util.Dom.get('post_form').submit();
+});
