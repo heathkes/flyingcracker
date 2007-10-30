8563bf6f43f3b10b03ecd849269829c132cd63a1
graham <devnull@localhost>
2007-10-30 01:37:50 +0000

    Yahoo UI Rich Text Editor now invoked for editing Post "body" in Django admin. BugzID: 4

---
+++ static/js/yuitextareas.js
@@ -0,0 +1,7 @@
+var myEditor = new YAHOO.widget.Editor('id_body', { 
+	    height: '600px', 
+	    width: '100%', 
+	    dompath: true, //Turns on the bar at the bottom 
+	    animate: true //Animates the opening, closing and moving of Editor windows 
+	}); 
+myEditor.render(); 
