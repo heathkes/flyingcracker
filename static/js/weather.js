2bdd125966eb486ba96c66efdcc3c57e0c8db4b8
graham <devnull@localhost>
2008-01-21 16:51:07 +0000

    show/hide weather units

--- static/js/weather.js
+++ static/js/weather.js
@@ -1,14 +1,4 @@
 var current_weather = {
-    init: function() {
-        var oButton = new YAHOO.widget.Button(
-            "title_toggle",  // Source element id
-            { 
-                type: "checkbox",
-                label: "Labels"
-            }
-        );
-        oButton.addListener("click", current_weather.title_toggle);
-    },
     
     title_toggle: function(e) {
         var elements = YAHOO.util.Dom.getElementsByClassName('curr_title', 'span');
@@ -22,6 +12,40 @@ var current_weather = {
                 elements[i].style.visibility = state;
             }
         }
+    },
+    
+    unit_toggle: function(e) {
+        var elements = YAHOO.util.Dom.getElementsByClassName('curr_units', 'span');
+        if (elements) {
+            var state = elements[0].style.display;
+            if (state == 'none')
+                state = '';
+            else
+                state = 'none';
+            for (i=0; i<elements.length; i++) {
+                elements[i].style.display = state;
+            }
+        }
+    },
+    
+    init: function() {
+        var titleButton = new YAHOO.widget.Button(
+            "title_toggle",  // Source element id
+            { 
+                type: "checkbox",
+                label: "Labels"
+            }
+        );
+        titleButton.addListener("click", current_weather.title_toggle);
+        
+        var unitButton = new YAHOO.widget.Button(
+            "unit_toggle",  // Source element id
+            { 
+                type: "checkbox",
+                label: "Units"
+            }
+        );
+        unitButton.addListener("click", current_weather.unit_toggle);
     }
 };
 YAHOO.util.Event.addListener(window, 'load', current_weather.init);
