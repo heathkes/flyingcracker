0a5e7aa2fde7c893a476dc5a5a3b6d285aee7823
graham <devnull@localhost>
2008-01-21 19:21:39 +0000

    iphone detection

--- weather/views.py
+++ weather/views.py
@@ -31,46 +31,51 @@ def current(request):
 def weather(request):
     # get latest weather reading
     current = Weather.objects.latest('timestamp')
-    show_titles = request.COOKIES.get("curr_weather_show_titles")
-    if show_titles == None:
-        show_titles = "hidden"
-    show_units = request.COOKIES.get("curr_weather_show_units")
-    if show_units == None:
-        show_units = "none"
-    xhr = request.GET.has_key('xhr')
-    if xhr:
-        # Note that the current Django JSON serializer cannot serialize a single object, just a queryset.
-        # Therefore we need to reference the object.__dict__ with the DjangoJSONEncoder.
-        # [  We expected to be able to call simplejson.dumps(current, mimetype=...)  ]
-        json = simplejson.dumps(current.__dict__, cls=DjangoJSONEncoder)
-        return HttpResponse(json, mimetype='application/javascript')
-    else:
-        # set wind string
-        if int(float(current.wind_speed)) < 1:
-            wind = "Calm"
+    
+    agent = request.META.get('HTTP_USER_AGENT')
+    if (agent and agent.find('iPhone') != -1) or request.GET.has_key('iphone'):
+        show_titles = request.COOKIES.get("curr_weather_show_titles")
+        if show_titles == None:
+            show_titles = "hidden"
+        show_units = request.COOKIES.get("curr_weather_show_units")
+        if show_units == None:
+            show_units = "none"
+        xhr = request.GET.has_key('xhr')
+        if xhr:
+            # Note that the current Django JSON serializer cannot serialize a single object, just a queryset.
+            # Therefore we need to reference the object.__dict__ with the DjangoJSONEncoder.
+            # [  We expected to be able to call simplejson.dumps(current, mimetype=...)  ]
+            json = simplejson.dumps(current.__dict__, cls=DjangoJSONEncoder)
+            return HttpResponse(json, mimetype='application/javascript')
         else:
-            wind = "%s<br />%d" % (wind_dir_to_english(current.wind_dir), current.wind_speed)
-        
-        # set barometric pressure trend string
-        trend = current.baro_trend
-        if trend > Decimal(0):
-            trend = "+%3.2f" % trend
-        else:
-            if trend < Decimal("-0.09"):
-                trend = '<span class="warning">%3.2f</span>' % trend
+            # set wind string
+            if int(float(current.wind_speed)) < 1:
+                wind = "Calm"
             else:
-                trend = "%3.2f" % trend
-
-        if request.META.get('REMOTE_ADDR') == USI_WAN:
-            indoor = current.temp_inside
-        else:
-            indoor = None
-        return render_to_response('weather/view.html', {'current' : current,
-                                                        'wind': wind,
-                                                        'trend': trend,
-                                                        'indoor': indoor,
-                                                        'show_titles': show_titles,
-                                                        'show_units': show_units})
+                wind = "%s<br />%d" % (wind_dir_to_english(current.wind_dir), current.wind_speed)
+            
+            # set barometric pressure trend string
+            trend = current.baro_trend
+            if trend > Decimal(0):
+                trend = "+%3.2f" % trend
+            else:
+                if trend < Decimal("-0.09"):
+                    trend = '<span class="warning">%3.2f</span>' % trend
+                else:
+                    trend = "%3.2f" % trend
+    
+            if request.META.get('REMOTE_ADDR') == USI_WAN:
+                indoor = current.temp_inside
+            else:
+                indoor = None
+            return render_to_response('weather/view.html', {'current' : current,
+                                                            'wind': wind,
+                                                            'trend': trend,
+                                                            'indoor': indoor,
+                                                            'show_titles': show_titles,
+                                                            'show_units': show_units})
+    else:
+        return render_to_response('weather/current_no_ajax.html', {'current' : current})
 
 dir_table = {
     'NNE': 22.5,
