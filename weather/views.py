e035a69147cbd7f844e61491c010c32d03144292
graham <devnull@localhost>
2008-02-08 04:27:01 +0000

    Improved the weather interface.

--- weather/views.py
+++ weather/views.py
@@ -68,7 +68,7 @@ def weather(request):
                 indoor = current.temp_inside
             else:
                 indoor = None
-            return render_to_response('weather/current_small.html', {'current' : current,
+            return render_to_response('weather/iphone.html', {'current' : current,
                                                             'wind': wind,
                                                             'trend': trend,
                                                             'indoor': indoor,
