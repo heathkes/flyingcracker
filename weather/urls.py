550e041929f57e8d6c4ae7319e50196a62b2b1d8
graham <devnull@localhost>
2008-02-18 01:09:15 +0000

    Added temperature chart underneath temp value in iphone version of current weather.

--- weather/urls.py
+++ weather/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('fc3.weather.views',
     (r'^current/$', 'current_no_ajax'),
+    (r'^plot/$', 'google_chart'),
     (r'^$', 'weather'),
 )
--- weather/views.py
+++ weather/views.py
@@ -5,9 +5,53 @@ from django.utils import simplejson
 from django.core.serializers.json import DjangoJSONEncoder
 from decimal import *
 import socket
+import datetime
+import math
+
+from pygooglechart import XYLineChart, Axis
 
 USI_WAN = socket.gethostbyname("usi.dyndns.org")
 
+def google_chart(request):
+    url = today_temp_chart()
+    return render_to_response('weather/plot.html', {'chart': url})
+
+def today_temp_chart():
+    temp_list, max, min = today_temps()
+    max = int(math.ceil(max/10)*10)
+    min = int(math.floor(min/10)*10)
+    chart = XYLineChart(280, 70, x_range=(0,24), y_range=(min, max))
+    data = temp_list
+    chart.add_data((0,1,2,3,4,5,6,7,8,9,10,12,13,14,15,16,17,18,19,20,21,22,23,24))
+    chart.add_data(data)
+    axis_left_index = chart.set_axis_range(Axis.LEFT, min, max)
+    axis_right_index = chart.set_axis_range(Axis.RIGHT, min, max)
+    axis_bottom_index = chart.set_axis_labels(Axis.BOTTOM, ['', '6a', '12', '6p', ''])
+    chart.set_axis_style(axis_left_index, 'B0B0B0')
+    chart.set_axis_style(axis_right_index, 'B0B0B0')
+    chart.set_axis_style(axis_bottom_index, 'B0B0B0')
+    chart.set_colours(['66CCFF'])
+    chart.set_line_style(0, 3)
+    return chart.get_url()
+
+def today_temps():
+    today = datetime.datetime(2008,2,17)    # forcing specific date for testing
+    qs = Weather.objects.filter(timestamp__gte=today).order_by('timestamp')
+    hour = -1
+    max = -100
+    min = 200
+    temps = []
+    for rec in qs:
+        if hour != rec.timestamp.hour:
+            hour = rec.timestamp.hour
+            t = rec.temp
+            temps.append(int(t))
+            if t > max:
+                max = t
+            if t < min:
+                min = t
+    return temps, max, min
+    
 def current_no_ajax(request):
     # get latest weather reading
     current = Weather.objects.latest('timestamp')
@@ -68,12 +112,16 @@ def weather(request):
                 indoor = current.temp_inside
             else:
                 indoor = None
+                
+            url = today_temp_chart()
+            
             return render_to_response('weather/iphone.html', {'current' : current,
                                                             'wind': wind,
                                                             'trend': trend,
                                                             'indoor': indoor,
                                                             'show_titles': show_titles,
-                                                            'show_units': show_units})
+                                                            'show_units': show_units,
+                                                            'chart': url,})
     else:
         return render_to_response('weather/current_no_ajax.html', {'current' : current})
 
