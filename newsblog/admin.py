6d912ec488b635e9311776c403ead40d6ba1c976
graham <devnull@localhost>
2008-09-26 19:19:16 +0000

    Removed newsblog app.

--- newsblog/admin.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from django.contrib import admin
-import fc3.newsblog.models as news
-
-
-class PostAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(news.Post, PostAdmin)
