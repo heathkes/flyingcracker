21c12adb0818c1bd94d6e8d34208a7db9e640a41
graham <devnull@localhost>
2008-10-10 05:46:51 +0000

    Forcing some ChartUrl attributes to be unique together.

--- weather/admin.py
+++ weather/admin.py
@@ -4,7 +4,7 @@ import fc3.weather.models as weather
 
 
 class ChartUrlAdmin(admin.ModelAdmin):
-    pass
+    unique_together = (("date", "data_type", "unit", "size", "plots"),)
 
 admin.site.register(weather.ChartUrl, ChartUrlAdmin)
 
