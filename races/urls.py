be7bd8f0325b77830126e4c49c2107eb602af206
graham <devnull@localhost>
2009-03-12 00:05:47 +0000

    Removed races app.

--- races/urls.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('fc3.fantasy.views',
-    url(r'^$',                              'root', name='fantasy-root'),
-    url(r'^race/(?P<id>[0-9]+)/$',          'race_detail', name='fantasy-race-detail'),
-    url(r'^series/(?P<id>[0-9]+)/board/$',  'leaderboard', name='fantasy-series-leaderboard'),
-    url(r'^series/(?P<id>[0-9]+)/$',        'series_detail', name='fantasy-series-detail'),
-)
