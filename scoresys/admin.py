81a06d8bfed2b6844fd6de9c4eab2f262f0e824d
graham <devnull@localhost>
2009-03-18 17:26:16 +0000

    Moved 'scoresys' app to bitbucket repository

--- scoresys/admin.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-from django.contrib import admin
-import scoresys.models as scoresys
-
-admin.site.register(scoresys.ScoringSystem)
-admin.site.register(scoresys.ResultPoints)
