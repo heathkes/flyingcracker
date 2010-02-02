b3ce912b7c5ea28da463c176345152155ed6a3b7
graham <devnull@localhost>
2010-02-02 07:09:56 +0000

    Improved admin display and filtering for Guess model.

--- fantasy/admin.py
+++ fantasy/admin.py
@@ -4,9 +4,24 @@ from django.contrib.contenttypes import generic
 import fantasy.models as fantasy
 
 
+class GuessAdmin(admin.ModelAdmin):
+    list_display = ('series', 'event', 'user', 'competitor', 'late_entry', 'timestamp')
+    list_display_links = ('competitor',)
+    list_filter = ('user', 'late_entry')
+
+    def series(self, obj):
+        return "%s" % obj.guess_for.series.__unicode__()
+
+    def event(self, obj):
+        return "%s" % obj.guess_for.__unicode__()
+
+    def competitor(self, obj):
+        return "%s" % obj.competitor
+    competitor.short_description = 'Guess'
+
 admin.site.register(fantasy.Series)
 admin.site.register(fantasy.Event)
 admin.site.register(fantasy.Competitor)
 admin.site.register(fantasy.Result)
-admin.site.register(fantasy.Guess)
+admin.site.register(fantasy.Guess, GuessAdmin)
 
