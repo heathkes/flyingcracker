6b3a8c42ec418e0fbeaa10ca495787eb65953784
graham <devnull@localhost>
2010-02-07 21:41:57 +0000

    Redirect to home page if the login page was reached from a logout action. bugzid:319

--- context_processors.py
+++ context_processors.py
@@ -30,8 +30,11 @@ def system_version(request):
 def login_url_with_redirect(request):
     from django.utils.http import urlquote
     from django.contrib.auth import REDIRECT_FIELD_NAME
+    from django.core.urlresolvers import reverse
     
     login_url = settings.LOGIN_URL
     path = urlquote(request.get_full_path())
+    if path == reverse('auth_logout'):
+        path = reverse('fc-home')
     url = '%s?%s=%s' % (settings.LOGIN_URL, REDIRECT_FIELD_NAME, path)
     return {'login_url': url}
