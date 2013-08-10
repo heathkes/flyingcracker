ee6a2441004d84f9f6f4da65dc65886bd4839abb
Heath <keslerheath01@gmail.com>
2013-08-10 16:27:44 -0600

    Removing unused code.

--- food/views.py
+++ food/views.py
@@ -37,13 +37,13 @@ def recipe_list(request, recipe_type=""):
                                      'recipe_type': recipe_type,
                                     })
         return render_to_response('food/recipe_list.html', c)
-    
+
 def recipe_detail(request, slug, recipe_type=""):
     r = get_object_or_404(Recipe, slug=slug)
-    
+
     all_recipes, all_foodstuff = get_all_lists(recipe_type)
     all_categories = Category.objects.all().order_by('title')
-    
+
     # get ingredients for this recipe
     ingredient_list = []
     for ingredient in r.ingredients.all().order_by('rank'):
@@ -66,8 +66,8 @@ def recipe_detail(request, slug, recipe_type=""):
         else:
             return render_to_response('food/iphone/recipe_initial.html', c)
     else:
-       return render_to_response('food/recipe_detail.html', c)
-    
+        return render_to_response('food/recipe_detail.html', c)
+
 def foodstuff_list(request, recipe_type=""):
     all_recipes, all_foodstuff = get_all_lists(recipe_type)
     agent = request.META.get('HTTP_USER_AGENT')
@@ -87,10 +87,10 @@ def foodstuff_list(request, recipe_type=""):
                                      'recipe_type': recipe_type,
                                     })
         return render_to_response('food/foodstuff_list.html', c)
-    
+
 def foodstuff_detail(request, slug, recipe_type=""):
     f = get_object_or_404(Foodstuff, slug=slug)
-    
+
     ingredient_list = f.ingredients.all().order_by('recipe')
     agent = request.META.get('HTTP_USER_AGENT')
     if (agent and agent.find('iPhone') != -1) or request.GET.has_key('iphone'):
@@ -113,7 +113,7 @@ def foodstuff_detail(request, slug, recipe_type=""):
                                      'recipe_type': recipe_type,
                                     })
         return render_to_response('food/foodstuff_detail.html', c)
-    
+
 def get_all_lists(recipe_type):
     all_recipes = Recipe.objects.filter(rclass=db_recipe_type(recipe_type)).order_by('title')
     all_foodstuffs = Foodstuff.objects.filter(ingredients__recipe__rclass=db_recipe_type(recipe_type)).distinct().order_by('title')
