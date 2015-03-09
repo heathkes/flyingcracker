6cd2f1316367c518fef583c032ec87916af918e1
Heath <keslerheath01@gmail.com>
2015-03-09 10:59:58 -0600

    PEP8 fixes, removing unused imports. [#89877374]

--- cam/urls.py
+++ cam/urls.py
@@ -1,5 +1,4 @@
-from django.conf.urls import patterns, url, include
-from django.contrib import admin
+from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('cam.views',
--- fc3/urls.py
+++ fc3/urls.py
@@ -1,9 +1,8 @@
 from __future__ import absolute_import
 
-from django.conf.urls import patterns, url, include
+from django.conf.urls import patterns, include
 from django.contrib import admin
 from django.conf import settings
-from fc3 import views
 
 admin.autodiscover()
 
@@ -12,38 +11,43 @@ try:
 except ImportError:
     pattern_views = None
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
+
     # Admin URLs
-    (r'^admin/doc/',                        include('django.contrib.admindocs.urls')),
-    (r'^admin/',                            include(admin.site.urls)),
-    (r'^comments/',                         include('django.contrib.comments.urls')),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/', include(admin.site.urls)),
+    (r'^comments/', include('django.contrib.comments.urls')),
 
     # Application URLs
-    (r'^accounts/',                         include('registration.urls')),
-#    (r'^profiles/',                         include('profiles_urls')),
-#    (r'^client/',                           include('serviceclient.urls')),
-    (r'^blog/',                             include('blog.urls')),
-    (r'^cam/',                              include('cam.urls')),
-    (r'^(?P<recipe_type>cocktail|food)/',   include('food.urls')),
-    (r'^weatherstation/',                   include('weatherstation.urls')),
-    (r'^weather/',                          include('weather.urls')),
-    (r'^grill/',                            include('grill.urls')),
-    (r'^miniblog/',                         include('miniblog.urls')),
-    (r'^fantasy/',                          include('fantasy.urls')),
-    (r'^',                                  include('home.urls')),
+    (r'^accounts/', include('registration.urls')),
+    (r'^blog/', include('blog.urls')),
+    (r'^cam/', include('cam.urls')),
+    (r'^(?P<recipe_type>cocktail|food)/', include('food.urls')),
+    (r'^weatherstation/', include('weatherstation.urls')),
+    (r'^weather/', include('weather.urls')),
+    (r'^grill/', include('grill.urls')),
+    (r'^miniblog/', include('miniblog.urls')),
+    (r'^fantasy/', include('fantasy.urls')),
+    (r'^', include('home.urls')),
 )
 
 if pattern_views and settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^patterns/$',                        pattern_views.show_url_patterns),
+    urlpatterns += patterns(
+        '',
+        (r'^patterns/$', pattern_views.show_url_patterns),
     )
 
-if settings.STATIC_URL[ :5] != 'http:':
-    urlpatterns += patterns('django.views.static',
-        (r'^' + settings.STATIC_URL + '(?P<path>.*)$', 'serve', {'document_root': settings.STATIC_ROOT }),
+if settings.STATIC_URL[:5] != 'http:':
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'^' + settings.STATIC_URL + '(?P<path>.*)$',
+         'serve', {'document_root': settings.STATIC_ROOT}),
     )
 
 if hasattr(settings, 'LOCAL_URL') and hasattr(settings, 'LOCAL_ROOT'):
-    urlpatterns += patterns('django.views.static',
-        (r'^' + settings.LOCAL_URL + '/(?P<path>.*)$', 'serve', {'document_root': settings.LOCAL_ROOT }),
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'^' + settings.LOCAL_URL + '/(?P<path>.*)$',
+         'serve', {'document_root': settings.LOCAL_ROOT}),
     )
