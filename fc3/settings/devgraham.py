52ba6cd28f854870fedd5c55513e477d3468588b
Heath <keslerheath01@gmail.com>
2015-04-22 13:46:38 -0600

    Adding secrets handling, improving settings files. Now requires secrets.json. [#93049528]

--- fc3/settings/devgraham.py
+++ fc3/settings/devgraham.py
@@ -1,13 +1,5 @@
 from __future__ import absolute_import
-import sys
-
-from unipath import Path
-import json
-#!/usr/bin/env python
 from .local import *
-
-
-DEBUG=True
 
 DATABASES = {
     'default': {
@@ -19,31 +11,5 @@ DATABASES = {
     }
 }
 
-# This setting describes the URL path where static content originates.
-# If in a Django development server environment, this should be 'static'.
-# If in a production environment the value will look like 'http://domainname/appname/static'.
-
-
-LOCAL_URL = 'local'
-
-BASE_DIR = Path(__file__).ancestor(3)
-
-LOCAL_ROOT = BASE_DIR.child("media")
-
-LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/'
-
-DEFAULT_FROM_EMAIL = "Graham Ullrich <graham@flyingcracker.com>"
-
-try:
-    EMAIL_USE_TLS = True
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = 'graham@flyingcracker.com'
-    EMAIL_HOST_PASSWORD = ''
-    EMAIL_PORT = 587
-except:
-    print "email not working"
-
-
-
-INTERNAL_IPS = '127.0.0.1'
+LOCAL_ROOT = "/Users/graham/Documents/fc3/testing/web/media"
+WEATHER_ROOT = LOCAL_ROOT + '/weather/'
--- fc3/settings/local.py
+++ fc3/settings/local.py
@@ -1,49 +1,16 @@
 from __future__ import absolute_import
-import sys
-
 from unipath import Path
-#!/usr/bin/env python
 from .base import *
-from .secrets import *
 
 DEBUG=True
 
-DATABASES = {
-    'default': {
-        'NAME': 'flyingcracker',
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'USER': 'johnevans',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-    }
-}
-
-# This setting describes the URL path where static content originates.
-# If in a Django development server environment, this should be 'static'.
-# If in a production environment the value will look like 'http://domainname/appname/static'.
-
-
 LOCAL_URL = 'local'
 
-BASE_DIR = Path(__file__).ancestor(5)
-
+BASE_DIR = Path(__file__).ancestor(3)
 LOCAL_ROOT = BASE_DIR.child("media")
 WEATHER_ROOT = LOCAL_ROOT + '/weather/'
 
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
-
-DEFAULT_FROM_EMAIL = "John Evans <johnevanswebbot@gmail.com>"
-
-try:
-    EMAIL_USE_TLS = True
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = 'johnevanswebbot@gmail.com'
-    EMAIL_HOST_PASSWORD = ''
-    EMAIL_PORT = 587
-except:
-    print "email not working"
-
-
 
 INTERNAL_IPS = '127.0.0.1'
