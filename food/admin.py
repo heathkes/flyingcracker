641777eef0414ebe729778cf94fca63dd674cdce
Heath <keslerheath01@gmail.com>
2015-05-01 00:28:44 -0600

    Fixing admin references for Django 1.7.
    [#93473948]

--- food/admin.py
+++ food/admin.py
@@ -1,20 +1,12 @@
 from django.contrib import admin
-from django.contrib.contenttypes import generic
 from django.db import models
 import food.models as food
 from django.conf import settings
-from django.contrib.contenttypes.fields import GenericForeignKey
-
-try:
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-except ImportError:
-    from django.contrib.auth.models import User
 
 
 class FoodstuffAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
-    
+
 admin.site.register(food.Foodstuff, FoodstuffAdmin)
 user = models.ForeignKey(settings.AUTH_USER_MODEL)
 admin.site.register(food.Attribute)
@@ -34,8 +26,10 @@ class IngredientInline(admin.TabularInline):
 class RecipeAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     fieldsets = (
-        (None, {'fields': ('title', 'slug', 'teaser', 'attributes', 'categories', 'rclass', 'pub_date')}),
-        ('directions, description, credit', {'fields' : ('directions', 'description', 'credit')}),
+        (None, {'fields': ('title', 'slug', 'teaser', 'attributes',
+                           'categories', 'rclass', 'pub_date')}),
+        ('directions, description, credit',
+         {'fields': ('directions', 'description', 'credit')}),
     )
     list_filter = ['rclass']
     inlines = [
@@ -45,16 +39,15 @@ class RecipeAdmin(admin.ModelAdmin):
 admin.site.register(food.Recipe, RecipeAdmin)
 
 
-class FoodstuffInline(generic.GenericTabularInline):
+class FoodstuffInline(admin.ModelAdmin):
     model = food.Foodstuff
-    user = generic.GenericForeignKey(settings.AUTH_USER_MODEL)
 
 
 class IngredientAdmin(admin.ModelAdmin):
     inlines = [
         FoodstuffInline,
     ]
-    
+
 admin.site.register(food.Ingredient, IngredientAdmin)
 
 admin.site.register(food.Link)
