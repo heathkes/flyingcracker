1ad6b9fcd2380586a05ffb4da8b36bfec8f11e5a
Heath <keslerheath01@gmail.com>
2015-05-27 13:42:50 -0600

    Restoring settings.
    [#95492348]

---
+++ flyingcracker/config/settings/devjake.py
@@ -0,0 +1,45 @@
+from __future__ import absolute_import
+
+from unipath import Path
+
+from .local import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'NAME': 'flyingcracker',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': 'johnevans',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+    }
+}
+
+DEFAULT_FROM_EMAIL = "John Evans <johnevanswebbot@gmail.com>"
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'johnevanswebbot@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+
+LOCAL_URL = 'local'
+
+BASE_DIR = Path(__file__).ancestor(3)
+
+LOCAL_ROOT = BASE_DIR.child("media")
+
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+
+DEFAULT_FROM_EMAIL = "John Evans <johnevanswebbot@gmail.com>"
+
+# Just dump emails to the console.
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# Set queue time to 0, so mail is sent instantly.
+NOTIFICATIONS_QUEUE_MINS = 0
+
+INSTALLED_APPS += [
+    'test_plus',
+]
---
+++ flyingcracker/config/settings/production.py
@@ -0,0 +1,46 @@
+from unipath import Path
+#!/usr/bin/env python
+from .base import *
+
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'NAME': 'graham_fc3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': 'graham_fc3',
+        'PASSWORD': '132af5e4',
+        'HOST': 'localhost',
+    }
+}
+
+# Where are media and weather files?
+MEDIA_DIR = STATIC_DIR = Path("/home/graham/webapps")
+MEDIA_ROOT = MEDIA_DIR.child("media")
+WEATHER_ROOT = MEDIA_ROOT.child('weather')
+STATIC_ROOT = STATIC_DIR.child('static')
+
+STATICFILES_DIRS = (
+    BASE_DIR.child('static'),
+)
+
+ALLOWED_INCLUDE_ROOTS = (BASE_DIR,)
+
+STATIC_URL = 'http://cracklyfinger.com/static/'
+MEDIA_URL = 'http://cracklyfinger.com/media/'
+
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
+ACCOUNT_ACTIVATION_DAYS = 10
+
+YUI_VERSION = "2.9.0"
+
+SYSTEM_NAME = "cracklyfinger.com"
+
+AUTH_PROFILE_MODULE = 'fcprofile.FCProfile'
+
+ALLOWED_HOSTS = ['www.cracklyfinger.com', 'cracklyfinger.com', '*']
+
