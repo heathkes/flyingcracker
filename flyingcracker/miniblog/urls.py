870c17ea8fbe806dd8b807f4acd841f3f5b52af7
Heath <keslerheath01@gmail.com>
2015-10-30 13:40:05 -0600

    Fixing urlconf view parameters. [#107069444]

--- flyingcracker/miniblog/urls.py
+++ flyingcracker/miniblog/urls.py
@@ -8,16 +8,18 @@ from .models import Post
 
 urlpatterns = [
     url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>[-\w]+)/$',
-        dates.DateDetailView.as_view(model=Post, date_field='pub_date'),
+        dates.DateDetailView.as_view,
+        {'model': Post, 'date_field': 'pub_date'},
         name='miniblog-detail'),
-       (r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$',
-        dates.DayArchiveView.as_view(model=Post)),
-       (r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/$',
-        dates.MonthArchiveView.as_view(model=Post)),
-       (r'^(?P<year>\d{4})/$',
-        dates.YearArchiveView.as_view(model=Post)),
+    url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$',
+        dates.DayArchiveView.as_view, {'model': Post}),
+    url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/$',
+        dates.MonthArchiveView.as_view, {'model': Post}),
+    url(r'^(?P<year>\d{4})/$',
+        dates.YearArchiveView.as_view, {'model': Post}),
     url(r'^$',
-        dates.ArchiveIndexView.as_view(model=Post, date_field='pub_date'),
+        dates.ArchiveIndexView.as_view,
+        {'model': Post, 'date_field': 'pub_date'},
         name='miniblog-archive'),
 ]
 
